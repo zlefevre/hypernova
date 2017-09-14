312186643a6672d027cc9d1d6d74d62661603b6b
zlefevre <zaklefevre@gmail.com>
2017-09-14 17:06:34 -0400

    Restoring to original versions

--- php/subscribe.php
+++ php/subscribe.php
@@ -1,33 +1,29 @@
 <?php
 
 	// MailChimp
-	$api_key = '776cf1f8452bd25d61ab61709ba824b2-us16';
-	$list_id = '3d8bc8c6e8'; 
+	$APIKey = '776cf1f8452bd25d61ab61709ba824b2-us16'; // change to your API key
+	$listID = '3d8bc8c6e8'; // change to your list id
 
-    $email   = $_POST['email'];
+	$email   = $_POST['email'];
 
-    // Let's start by including the MailChimp API wrapper
-    include('./inc/MailChimp.php');
-    // Then call/use the class
-    use \DrewM\MailChimp\MailChimp;
-    $MailChimp = new MailChimp($api_key);
- 
-    // Submit subscriber data to MailChimp
-    // For parameters doc, refer to: http://developer.mailchimp.com/documentation/mailchimp/reference/lists/members/
-    // For wrapper's doc, visit: https://github.com/drewm/mailchimp-api
-    $result = $MailChimp->post("lists/$list_id/members", [
-                            'email_address' => $_POST["email"],
-                            'merge_fields'  => ['FNAME'=>$_POST["fname"], 'LNAME'=>$_POST["lname"]],
-                            'status'        => 'subscribed',
-                        ]);
- 
-    if ($MailChimp->success()) {
-        // Success message
-        echo "<h4>Thank you, you have been added to our mailing list.</h4>";
-    } else {
-        // Display error
-        echo $MailChimp->getLastError();
-        // Alternatively you can use a generic error message like:
-        // echo "<h4>Please try again.</h4>";
-    }
+	require_once('MCAPI.class.php');
+
+	$api = new MCAPI($APIKey);
+	$list_id = $listID;
+
+	if($api->listSubscribe($list_id, $email) === true) {
+		$sendstatus = 1;
+		$message = '<div class="alert alert-success subscription-success" role="alert"><strong>Success!</strong> Check your email to confirm sign up.</div>';
+	} else {
+		$sendstatus = 0;
+		$message = '<div class="alert alert-danger subscription-error" role="alert"><strong>Error:</strong> ' . $api->errorMessage.'</div>';
+	}
+
+	$result = array(
+		'sendstatus' => $sendstatus,
+		'message' => $message
+	);
+
+	echo json_encode($result);
+
 ?>
